4a96db1727f63dd54cf8d56b207e5575a05dda58
RDGray <70549101+RDGray@users.noreply.github.com>
2021-09-29 02:38:32 +0300

    Navigation bar layout
    Added the main upper navigation bar

--- app/js/modal-and-pin.js
+++ app/js/modal-and-pin.js
@@ -1,5 +1,10 @@
 const add_pin_modal = document.querySelector(".main");
 
 document
-  .querySelector(".add-pin__upload-pin-container")
-  .addEventListener("click", () => {});
+  .querySelector(".add-pin__upload-pin-create")
+  .addEventListener("click", () => {
+    add_pin_modal.style.opacity = 1;
+    add_pin_modal.style.pointerEvents = "all";
+    console.log(`zzz`);
+  });
+console.log(document.querySelector(".add-pin__upload-pin-container"));
